cd13700140a5d87038049224d7b665b1863440c6
jisung0920 <jisung0920@gmail.com>
2017-02-20 02:09:57 +0900

    write
    +시간 상세 설정
    +사용자 프로필 가져오기
    +게시 시 확인 응답

---
+++ 수정본_7/JiManagement/app/src/main/java/com/example/park/management/Free.java
@@ -0,0 +1,51 @@
+package com.example.park.management;
+
+/**
+ * Created by jisung on 2017. 2. 11..
+ */
+
+public class Free {
+    String title;
+    String name;
+    String date;
+    String data;
+
+    public Free(String title, String data,String name, String date) {
+        this.title = title;
+        this.name = name;
+        this.data = data;
+        this.date = date;
+    }
+
+    public String getData() {
+        return data;
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+}
---
+++ 수정본_7/JiManagement/app/src/main/java/com/example/park/management/timerAlpa.java
@@ -0,0 +1,57 @@
+package com.example.park.management;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+import java.util.Locale;
+
+/**
+ * Created by jisung on 2017. 2. 19..
+ */
+
+public class timerAlpa {
+
+    public long Counter(String target){
+        long a = doDiffOfDate(target,doCurrentDate());
+        long b = doDiffOfDate(doCurrentDate(),target);
+        if(a>b)
+            return a;
+        return b;
+    }
+
+    public long doDiffOfDate(String start, String end){
+        //String start = "2015-04-01"
+        try {
+            SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
+            Date beginDate = formatter.parse(start);
+            Date endDate = formatter.parse(end);
+
+            long diff = endDate.getTime() - beginDate.getTime();
+            long diffDays = diff / (24 * 60 * 60 * 1000);
+
+            return diffDays;
+
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        return 0;
+    }
+
+    public String doCurrentDate(){
+        int nYear;
+        int nMonth;
+        int nDay;
+
+        // 현재 날짜 구하기
+        Calendar calendar = new GregorianCalendar(Locale.KOREA);
+        nYear = calendar.get(Calendar.YEAR);
+        nMonth = calendar.get(Calendar.MONTH) + 1;
+        nDay = calendar.get(Calendar.DAY_OF_MONTH);
+        String strD =nYear + "-"+ nMonth + "-" + nDay;
+        return strD;
+    }
+
+}
+
